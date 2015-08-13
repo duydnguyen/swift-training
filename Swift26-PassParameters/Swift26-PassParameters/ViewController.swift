ae387883ffb8d4927181705921dbcd5b31028e79
Trần Đức Hiếu <tranduchieu@gmail.com>
2015-08-13 16:03:56 +0700

    Submit on Aug 13th 2015

---
+++ Swift26-PassParameters/Swift26-PassParameters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift26-PassParameters
+//
+//  Created by Tran Duc Hieu on 8/13/15.
+//  Copyright © 2015 Duy-Hieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
